0aa808bb2cdd3d58da8142b322269d48cd95482f
unknown <Ashu Lekhi@3804G-AshuL.gle.in>
2022-09-13 10:08:30 +0530

    first commit

---
+++ user/index.js
@@ -0,0 +1,57 @@
+const Express = require("express")
+const UserController = require("./user.controller")
+const jwt = require("jsonwebtoken")
+
+const router = Express.Router()
+
+router.post("/register", UserController.sandesh )
+router.get("/acquireloginstatus",function(req,res){
+    var token = req.get("Authorization")
+    try{
+        var payload = jwt.verify(token,"mysecretkey")
+        if(payload){
+            res.send()
+        }
+    }
+    catch(error){
+        res.status(500).send()
+    }
+})
+router.post("/login", UserController.login )
+router.put("/updateprofile",function(req,res,next){
+    var token = req.get("Authorization")
+    console.log("token" , token)
+    try{
+        var payload = jwt.verify(token,"mysecretkey")
+    }
+    catch {
+        console.log("token is not valid")
+        res.status(401).send()
+    }
+    if(payload){
+        req.body.email = payload.email
+        next()
+    }
+    console.log("token verification result" , payload)
+},UserController.updateProfile)
+router.get("/search",UserController.search)
+router.post("/uploadprofileimage",UserController.uploadProfileImage)
+router.delete("/deleteaccount", function(req,res,next){
+    var token = req.get("Authorization")
+    console.log("token" , token)
+    try{
+        var payload = jwt.verify(token,"mysecretkey")
+    }
+    catch {
+        console.log("token is not valid")
+        res.status(401).send()
+    }
+    if(payload){
+        req.body.email = payload.email
+        next()
+    }
+    console.log("token verification result" , payload)
+} ,UserController.deleteAccount)
+router.post("/recoverpassword",UserController.recoverPassword)
+
+module.exports = router 
